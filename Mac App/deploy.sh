d3e8ee46e627919e33a020fef1a4b256ea274f55
Maximilian Mitchell <max@maxis.me>
2018-03-24 15:07:34 +0000

    Update deploy script to work with my deploy tools.

--- Mac App/deploy.sh
+++ Mac App/deploy.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
+project_name="Transfer Me It"
+domain="transferme.it"
+work_name="transfermeit"
+project_type=".xcworkspace"
+
+###################
 
 dir=$(pwd)
-project_dir="${dir}/Documents/work/transfermeit/"
+project_dir="${dir}/Documents/work/${work_name}/"
 deployment_dir="${dir}/Documents/work/App Deployment/Deployer/"
 
-project_name="Transfer Me It"
-project_type=".xcworkspace"
+#app
 project_path="${project_dir}Mac App/"
 dev_team="3H49MXS325"
-dmg_project_output="${project_dir}transferme.it/public_html/${project_name}.dmg"
-scp_command="scp '"$dmg_project_output"' root@transferme.it:/var/www/transferme.it/public_html/"
-sparkle_path="https://transferme.it/version.php"
 # `security find-identity -v -p codesigning` ->> "Mac Developer"
 # can be recreated with xcode preferences > + > mac Developer
 sign_key="92FE8FE7E7A291030E292B8129AD99F72E65F585"
 
+#dmg
+dmg_project_output="${project_dir}${domain}/public_html/${project_name}.dmg"
+
+#server
+scp_command="scp '"$dmg_project_output"' root@${domain}:/var/www/${domain}/public_html/"
+sparkle_path="https://${domain}/version.php"
+
+#run
 cd "$deployment_dir"
 bash deployer.sh "$project_name" "$project_type" "$project_path" "$dev_team" "$dmg_project_output" "$scp_command" "$sparkle_path" "$sign_key"
